496aea64d781b76bf0e6d37ffb2587f2d976649e
Etudiant <Etudiant@MAESTRISLILLE.LAN>
2024-07-23 15:25:57 +0200

    (+) Video added

--- js/video.js
+++ js/video.js
@@ -5,7 +5,24 @@ class Video {
 
     loadVideo() {
         const video = document.querySelectorAll("#video > video");
-        console.log(video);
+        const button = document.querySelector("#video > .video-overlay > #video-play");
+        button.addEventListener("click", () => {
+            if(video[0].paused) {
+                video[0].play();
+                button.innerHTML = "<i class='fa-solid fa-pause'></i>";
+            } else {
+                video[0].pause();
+                button.innerHTML = "<i class='fa-solid fa-play'></i>";
+            }
+        });
+
+        const restartButton = document.querySelector("#video > .video-overlay > #video-reset");
+
+        restartButton.addEventListener("click", () => {
+            video[0].currentTime = 0;
+            video[0].pause();
+            button.innerHTML = "<i class='fa-solid fa-play'></i>";
+        });
     }
 }
 
